cb8f3dd8187606ef5885f755977a459886038e3a
Ljojua15 <jojualasha15@gmail.com>
2024-10-02 11:12:45 +0400

    '6'

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-console.log("aaa");
+//1.გამოაცხადეთ ცვლადი "userInput" რომელსაც მიანიჭებთ მხოლოდ „number"-სა და „string"-ს.
+//შექმენით ფუნქცია processinput(), რომელიც მიიღებს user input -ს.
+//თუ user input რიცხვია, კონსოლში გამოვიტანოთ ლუწია თუ არა.
+//b. თუ სტრინგი, მაშინ მისი სიგრძე
+
+let userInput: number | string = 1;
+
+function processinput(userInput: string | number) {
+  if (typeof userInput === "number") {
+    console.log(userInput % 2 === 0 ? "ლუწი" : "კენტი");
+  } else {
+    console.log(userInput.length);
+  }
+}
+
+// processinput(userInput);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//2.
